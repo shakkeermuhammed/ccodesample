7b75f2a5bc47d6070ef61dffceab120cb8895243
Muhammed Shakkeer <shakkeer.muhammed@gmail.com>
2020-12-23 11:27:31 +0530

    Module and app - character driver to read/write memory

---
+++ LinuxSystemProgramming/Module-CharDriver-KM/char_memory/app2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main()
+{
+
+int fd,ret;
+char buff[10];
+
+	fd = open("/dev/memory",O_RDWR);
+	if (fd < 0)
+	perror("open FAILS\n");
+	
+	ret = write(fd,"SAIF",4);
+	printf("write return value:%d\n",ret);
+
+        getchar();
+	
+	ret = read(fd,buff,100);
+	printf("read return value:%d buff[0]:%s \n",ret,buff);
+
+	close(fd);
+}
